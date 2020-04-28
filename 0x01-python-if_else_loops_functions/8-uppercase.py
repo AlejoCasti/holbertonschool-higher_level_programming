30bd649fc3932dec09112f8bd0e9829c6f5624d2
AlejoCasti <acastiblanco8@gmail.com>
2020-04-28 18:08:30 +0000

    [FIX]

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -4,8 +4,5 @@ def uppercase(str):
         asci = ord(str[a])
         if asci >= 97 and asci <= 122:
             asci -= 32
-        if a == len(str) - 1:
-            print(chr(asci))
-        else:
-            print("{0}".format(chr(asci)), end='')
+        print("{0}".format(chr(asci)), end='')
     print("")
