38157a687154b3ce23177449cd07f1a713c8fd18
AlejoCasti <acastiblanco8@gmail.com>
2020-04-28 17:47:44 +0000

    [FIX] code issue

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 def uppercase(str):
-    for a in range(len(str)):
-        asci = ord(str[a])
-        if asci >= 97 and asci <= 122:
-            asci -= 32
-        if a == len(str) - 1:
-            print(chr(asci))
-        else:
-            print("{0}".format(chr(asci)), end='')
+    if str is not None:
+        for a in range(len(str)):
+            asci = ord(str[a])
+            if asci >= 97 and asci <= 122:
+                asci -= 32
+            if a == len(str) - 1:
+                print(chr(asci))
+            else:
+                print("{0}".format(chr(asci)), end='')
