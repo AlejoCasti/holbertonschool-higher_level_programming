ccbceb38c7d7c89137126495b6aec7cd038ff2e6
AlejoCasti <acastiblanco8@gmail.com>
2020-05-05 21:19:54 +0000

    [ADD]

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,43 @@
+#include "lists.h"
+/**
+ * get_result - verify if is palindrome word
+ * @head: first node of linked list
+ * @a: module of list length
+ * @middle: middle of linked
+ * Return: 1 is palindrome 0 is not
+ */
+int get_result(listint_t **head, int a, int middle)
+{
+	listint_t *ptr = *head;
+	int i, sum = 0;
+
+	for (i = 0; ptr; i++)
+	{
+		if (a == 1 && i == middle)
+			continue;
+		if (i < middle)
+			sum += ptr->n;
+		if (i >= middle)
+			sum -= ptr->n;
+		ptr = ptr->next;
+	}
+	if (sum != 0)
+		return (0);
+	else
+		return (1);
+}
+/**
+ * is_palindrome - verify if is palindrome word
+ * @head: first node of linked list
+ * Return: 1 is palindrome 0 is not
+ */
+int is_palindrome(listint_t **head)
+{
+	listint_t *ptr = *head;
+	int counter = 0;
+
+	while (ptr)
+		ptr = ptr->next, counter++;
+	return (get_result(head, counter % 2, counter / 2));
+
+}
