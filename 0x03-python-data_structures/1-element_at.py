63f48dfe04f700e1de4a25e3f00768db0ee316c8
AlejoCasti <acastiblanco8@gmail.com>
2020-05-05 17:10:46 +0000

    [ADD]

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    retorn = None
     if idx < 0:
         return None
-    retorn = [i for index, i in enumerate(my_list) if index == idx]
-    return retorn[0]
+    for index, i in enumerate(my_list):
+        if index == idx:
+            return i
+    return None
