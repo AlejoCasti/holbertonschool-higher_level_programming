a3ee52eb12ee85b134d56f4e72f931b8f9fc92f0
AlejoCasti <acastiblanco8@gmail.com>
2020-05-05 21:27:51 +0000

    [ADD]

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -36,10 +36,10 @@ int is_palindrome(listint_t **head)
 	listint_t *ptr = *head;
 	int counter = 0;
 
-	if (!*head)
-		return (1);
 	while (ptr)
 		ptr = ptr->next, counter++;
+	if (counter == 1)
+		return (1);
 	return (get_result(head, counter % 2, counter / 2));
 
 }
