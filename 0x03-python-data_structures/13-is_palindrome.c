3d7bd2d9c725fcb7c12ddf9e2b4d3888e462aa6a
AlejoCasti <acastiblanco8@gmail.com>
2020-05-05 21:33:04 +0000

    [ADD]

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -14,10 +14,13 @@ int get_result(listint_t **head, int a, int middle)
 	for (i = 0; ptr; i++)
 	{
 		if (a == 1 && i == middle)
+		{
+			ptr = ptr->next;
 			continue;
+		}
 		if (i < middle)
 			sum += ptr->n;
-		if (i >= middle)
+		else if (i >= middle)
 			sum -= ptr->n;
 		ptr = ptr->next;
 	}
