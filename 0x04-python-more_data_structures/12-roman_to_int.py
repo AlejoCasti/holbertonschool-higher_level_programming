e58e65157c0b4920d076d4405416fa1aeb93a908
AlejoCasti <acastiblanco8@gmail.com>
2020-05-07 22:31:58 +0000

    [ADD] 12 task

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    suma = 0
+    for i in roman_string:
+        suma += 1 if i == 'I' else 0
+        suma += 5 if i == 'V' else 0
+        suma += 10 if i == 'X' else 0
+        suma += 50 if i == 'L' else 0
+        suma += 100 if i == 'C' else 0
+        suma += 500 if i == 'D' else 0
+        suma += 1000 if i == 'M' else 0
+    return suma
