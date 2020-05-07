040cdc634f07deaea1ce95e9f5e40f98ec387e74
AlejoCasti <acastiblanco8@gmail.com>
2020-05-07 22:11:26 +0000

    [ADD] 10 task

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,5 +2,6 @@
 def best_score(a_dictionary):
     max_key = None
     if a_dictionary:
-        max_key = max(a_dictionary)
+        max_va = max(list(a_dictionary.values()))
+        max_key = [i for i in a_dictionary if a_dictionary[i] == max_va][0]
     return max_key
