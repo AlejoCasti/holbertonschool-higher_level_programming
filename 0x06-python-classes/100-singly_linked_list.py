8f56394f28c8dc1db331b73290368c847006e8cc
AlejoCasti <acastiblanco8@gmail.com>
2020-05-19 18:57:28 +0000

    [ADD] advanced task

---
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python3
+class Node:
+    def __init__(self, data, next_node=None):
+        if type(data) is not int:
+            raise TypeError('data must be an integer')
+        if type(next_node) is not Node and next_node is not None:
+            raise TypeError('next_node must be a Node object')
+        self.__data = data
+        self.__next_node = next_node
+
+    # Getter and Setter
+    # data
+    @property
+    def data(self):
+        return self.__data
+
+    @data.setter
+    def data(self, value):
+        if type(value) is not int:
+            raise TypeError('data must be an integer')
+        self.__data = value
+
+    # next_node
+    @property
+    def next_node(self):
+        return self.__next_node
+
+    @next_node.setter
+    def next_node(self, value):
+        if type(value) is not Node and value is not None:
+            raise TypeError('next_node must be a Node object')
+        self.__next_node = value
+
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.__head = None
+
+    def __str__(self):
+        tmp_node = self.__head.next_node
+        lista = str(self.__head.data)
+        while tmp_node is not None:
+            lista += "\n" + str(tmp_node.data)
+            tmp_node = tmp_node.next_node
+        return lista
+
+    def sorted_insert(self, value):
+        new_node = Node(value)
+        tmp_node = self.__head
+        counter = 0
+        if self.__head is None:
+            self.__head = new_node
+            return
+        else:
+            while tmp_node.next_node is not None:
+                if counter == 0 and tmp_node.data > value:
+                    new_node.next_node = self.__head
+                    self.__head = new_node
+                    return
+                if (tmp_node.data <= value) and\
+                   (tmp_node.next_node is None
+                        or tmp_node.next_node.data > value):
+                    new_node.next_node = tmp_node.next_node
+                    tmp_node.next_node = new_node
+                    return
+                tmp_node = tmp_node.next_node
+                counter += 1
+            new_node.next_node = tmp_node.next_node
+            tmp_node.next_node = new_node
