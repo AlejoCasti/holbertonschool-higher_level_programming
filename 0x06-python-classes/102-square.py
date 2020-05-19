7cd5a949628d23df7aad97fc90f66657462b1fce
AlejoCasti <acastiblanco8@gmail.com>
2020-05-19 19:24:31 +0000

    [ADD] advanced task

---
+++ 0x06-python-classes/102-square.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+class Square:
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
+
+    # Compare functions
+    def area(self):
+        return self.__size * 2
+
+    def __lt__(self, other):
+        return self.area() < other.area()
+
+    def __le__(self, other):
+        return self.area() <= other.area()
+
+    def __eq__(self, other):
+        return self.area() == other.area()
+
+    def __ne__(self, other):
+        return self.area() != other.area()
+
+    def __gt__(self, other):
+        return self.area() > other.area()
+
+    def __ge__(self, other):
+        return self.area() >= other.area()
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError('size must be an integer')
+        if value < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = value
