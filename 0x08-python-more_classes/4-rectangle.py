0cb06fbcf45155bd65a9a5678983577df260f7b9
AlejoCasti <acastiblanco8@gmail.com>
2020-05-25 23:34:31 +0000

    [ADD] project

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -45,6 +45,8 @@ class Rectangle:
             return n_s
         for i in range(self.height):
             n_s += ('#' * self.width) + '\n'
+            if i != self.height - 1:
+                n_s += '\n'
         return n_s
 
     def __repr__(self):
