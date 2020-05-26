d16f672e651701f7787edba0e8f26c2171cfd12d
AlejoCasti <acastiblanco8@gmail.com>
2020-05-26 01:13:13 +0000

    [ADD] project

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -57,7 +57,7 @@ class Rectangle:
 
     def __del__(self):
         Rectangle.number_of_instances -= 1
-        print('Bye rectangle...')
+        print('Bye rectangle....')
 
     """ public instance method """
     def area(self):
