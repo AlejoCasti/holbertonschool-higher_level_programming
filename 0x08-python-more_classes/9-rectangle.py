56382928be510ccc361bc36ebd27f53aba63e46c
AlejoCasti <acastiblanco8@gmail.com>
2020-05-26 01:23:34 +0000

    [ADD] project

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -10,9 +10,9 @@ class Rectangle:
 
     def __init__(self, width=0, height=0):
         """ Initialize rectangle """
-        Rectangle.number_of_instances += 1
         self.width = width
         self.height = height
+        Rectangle.number_of_instances += 1
 
     @classmethod
     def square(cls, size=0):
