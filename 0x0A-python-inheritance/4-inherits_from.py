32122b104285b147e084a7eab0bdc0b40b1bd044
AlejoCasti <acastiblanco8@gmail.com>
2020-06-03 00:43:51 +0000

    [FIX] issubclass function!

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
+""" documentation """
+
+
 def inherits_from(obj, a_class):
-    if isinstance(obj, a_class):
+    """ Check if is instance """
+    if type(obj) != a_class and issubclass(type(obj), a_class):
         return True
     else:
         return False
