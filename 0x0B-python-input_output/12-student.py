02bba306a4f14ce27f63148fbca25bf4e8de0851
AlejoCasti <acastiblanco8@gmail.com>
2020-06-03 22:42:00 +0000

    [ADD] project

--- 0x0B-python-input_output/12-student.py
+++ 0x0B-python-input_output/12-student.py
@@ -5,12 +5,14 @@
 class Student:
     """ class Student """
     def __init__(self, first_name, last_name, age):
+        """ """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        if attrs is None or type(attrs) is not list\
+        """ """
+        if type(attrs) is not list\
            or len(set([type(j) for j in attrs])) != 1:
             return self.__dict__
         else:
