74c5d47dab1712474ff1e6dbe2af85ddcd8651f2
AlejoCasti <acastiblanco8@gmail.com>
2020-06-04 04:42:28 +0000

    [FIX] advanced task

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -4,14 +4,14 @@
 
 def append_after(filename="", search_string="", new_string=""):
     """ append_after function """
+    if new_string[-1] == '\n':
+        new_string = new_string[:-1]
     with open(filename, mode='r') as fil:
         con = fil.read().split('\n')
         new = []
         for i in con:
             new.append(i)
             if search_string in i:
-                if new_string[-1] == '\n':
-                    new_string = new_string[:-1]
                 new.append(new_string)
     with open(filename, mode='w') as f:
         f.write("\n".join(new))
