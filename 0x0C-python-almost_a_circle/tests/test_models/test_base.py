031dabb88cb001ed792355df3dbd9ef4ef910b29
AlejoCasti <acastiblanco8@gmail.com>
2020-06-10 04:48:12 +0000

    [ADD] project

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,37 +1,89 @@
 #!/usr/bin/python3
+"""
+Unittest for Base class
+"""
+
 import unittest
 from models.base import Base
+from models.rectangle import Rectangle
+from models.square import Square
+import io
+import sys
+import os
 
-class testBase(unittest.TestCase):
-    
-    def test_add(self):
-        ex = Base()
-        self.assertEqual(ex.id, 1)
-        ex = Base(38)
-        self.assertEqual(ex.id, 38)
-        ex = Base()
-        self.assertEqual(ex.id, 2)
-        ex = Base(3.2)
-        self.assertEqual(ex.id, 3.2)
-        ex = Base(-1)
-        self.assertEqual(ex.id, -1)
-        ex = Base([1, 2, 3])
-        self.assertEqual(ex.id, [1, 2, 3])
-        ex = Base(True)
-        self.assertEqual(ex.id, True)
-        ex = Base({'pru': 1})
-        self.assertEqual(ex.id, {'pru': 1})
-        ex = Base((1, ))
-        self.assertEqual(ex.id, (1, ))
-        ex = Base()
-        self.assertEqual(ex.id, 3)
 
-    def test_tjs(self):
-        self.assertEqual(Base.to_json_string(None), '[]')
-        self.assertEqual(Base.to_json_string([]), '[]')
-        failure_msg = 'object of type \'int\' has no len()'
-        self.assertTrue(Base.to_json_string(1), msg=failure_msg)
-        
+class TestBase(unittest.TestCase):
+    """
+    class TestBase
+    """
 
-if __name__ == '__main__':
-    unittest.main()
+    def test_class(self):
+        b0 = Base()
+        self.assertTrue(isinstance(b0, Base))
+
+    def test_nb(self):
+        b0 = Base()
+        self.assertTrue(hasattr(b0, '_Base__nb_objects'))
+        Base._Base__nb_objects = 0
+
+    def test_id(self):
+        b0 = Base()
+        self.assertTrue(hasattr(b0, 'id'))
+        Base._Base__nb_objects = 0
+
+    def test_automatic(self):
+        b1 = Base()
+        self.assertEqual(b1.id, 1)
+        b2 = Base()
+        self.assertEqual(b2.id, 2)
+        b3 = Base()
+        self.assertEqual(b3.id, 3)
+        b4 = Base()
+        self.assertEqual(b4.id, 4)
+        b5 = Base()
+        self.assertEqual(b5.id, 5)
+        Base._Base__nb_objects = 0
+
+    def test_manual(self):
+        b1 = Base(45)
+        self.assertEqual(b1.id, 45)
+        b2 = Base(56)
+        self.assertEqual(b2.id, 56)
+        b3 = Base(67)
+        self.assertEqual(b3.id, 67)
+        b4 = Base(78)
+        self.assertEqual(b4.id, 78)
+        b5 = Base(89)
+        self.assertEqual(b5.id, 89)
+        Base._Base__nb_objects = 0
+
+    def test_mixed(self):
+        b1 = Base()
+        self.assertEqual(b1.id, 1)
+        b2 = Base(56)
+        self.assertEqual(b2.id, 56)
+        b3 = Base()
+        self.assertEqual(b3.id, 2)
+        b4 = Base(78)
+        self.assertEqual(b4.id, 78)
+        b5 = Base()
+        self.assertEqual(b5.id, 3)
+        Base._Base__nb_objects = 0
+
+    def tearDown(self):
+        Base._Base__nb_objects = 0
+
+    def test_base_instance(self):
+        obj = Base()
+        self.assertIsInstance(obj, Base)
+
+    def test_base_attr_id_exists(self):
+        obj = Base()
+        failure_msg = 'property id not found'
+        self.assertTrue(hasattr(obj, 'id'), msg=failure_msg)
+
+    def test_base_nb_objects_exists(self):
+        obj = Base()
+        failure_msg = 'global var __nb_objects not found'
+        self.assertTrue(hasattr(obj, '_Base__nb_objects'), msg=failure_msg)
+
