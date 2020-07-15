a3d153f73821ffdd248d6bf2348b263f723da725
AlejoCasti <acastiblanco8@gmail.com>
2020-07-15 04:08:50 +0000

    [ADD] 11 task

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,4 @@
+-- Write a script that lists all shows contained in the database hbtn_0d_tvshows
+SELECT ts.title, tsg.genre_id FROM tv_show_genres AS tsg
+RIGHT JOIN tv_shows AS tsON tsg.show_id = ts.id
+ORDER BY ts.title ASC, tsg.genre_id;
