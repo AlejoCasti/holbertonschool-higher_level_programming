731364bb4c3e8983287f64ec65b525d9d79f8984
AlejoCasti <acastiblanco8@gmail.com>
2020-08-09 07:07:16 +0000

    [ADD] 3 task

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+''' Script that lists all states form database '''
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+
+    arg = sys.argv
+    db = MySQLdb.connect(host='localhost', port=3306, user=arg[1],
+                         password=arg[2], database=arg[3])
+    cursor = db.cursor()
+    query = "SELECT * FROM states WHERE BINARY name =\
+    '{}' OR 1=1 ORDER BY id ASC ".format(sys.argv[4])
+    cursor.execute(query)
+    records = cursor.fetchall()
+    for row in records:
+        print(row)
+    cursor.close()
+    db.close()
