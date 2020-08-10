cfd17ca1d1d95da38e61c04a3765493f7f425f83
AlejoCasti <acastiblanco8@gmail.com>
2020-08-10 02:09:58 +0000

    [ADD] advanced task

---
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+''' Script that lists all states form database '''
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from relationship_state import State
+from relationship_city import City
+
+
+if __name__ == '__main__':
+    engine = create_engine('mysql://{}:{}@localhost/{}'.
+                           format(argv[1], argv[2], argv[3]))
+    sesion = sessionmaker(bind=engine)
+    session = sesion()
+    new_state = State(name='California')
+    new_city = City(name='San Francisco', state=new_state)
+    session.add(new_city)
+    session.commit()
+    session.close()
---
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+''' Class definition '''
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+Base = declarative_base()
+
+
+class State(Base):
+    ''' Class base '''
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)    
+    cities = relationship('City', backref='state', cascade="all, delete")
+
