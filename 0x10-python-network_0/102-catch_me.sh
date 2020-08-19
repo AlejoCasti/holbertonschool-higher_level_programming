522f9c845eb6195da39cf0b2f187ee91cc6a2656
AlejoCasti <acastiblanco8@gmail.com>
2020-08-19 04:04:42 +0000

    [ADD] advanced task

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Send a POST request with some parameters
-curl -s -X PUT -d "Allow=GET" -L $1 -d "user_id=98" -H "Origin: HolbertonSchool"
+curl -s -X PUT -d Allow=GET -L $1 -d user_id=98 -H "Origin: HolbertonSchool"
