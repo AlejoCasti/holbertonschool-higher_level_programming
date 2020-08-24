d7c2b351afe83713a394e48025f7df567a646e39
AlejoCasti <acastiblanco8@gmail.com>
2020-08-24 17:39:45 +0000

    [ADD] 8 task

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+''' Script that fetches web page '''
+
+if __name__ == '__main__':
+    import requests
+    from sys import argv
+    url = 'http://0.0.0.0:5000/search_user'
+    if len(argv) > 1:
+        data = {'q': argv[1]}
+    else:
+        data = {'q': ""}
+    response = requests.post(url, data)
+    js = response.json()
+    if js == {}:
+        print('No result')
+    else:
+        print('[{}] {}'.format(js.get('id'), js.get('name')))
