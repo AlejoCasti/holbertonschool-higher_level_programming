df90361cb5aa095ba3e53e9e43d54d457bdbb16e
AlejoCasti <acastiblanco8@gmail.com>
2020-09-23 13:13:49 -0500

    [ADD] tasks

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const request = require('request');
+request.get(`https://swapi-api.hbtn.io/api/films/${process.argv[2]}`, function (error, response, body) {
+  if (error === null) {
+    const { title } = JSON.parse(body)
+    console.log(title);
+  } else {
+    console.log(error);
+  }
+});
