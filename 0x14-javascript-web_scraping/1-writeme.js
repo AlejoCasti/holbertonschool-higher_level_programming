524b40a4929a33641589f201ab2f3225200bda58
AlejoCasti <acastiblanco8@gmail.com>
2020-09-23 13:16:22 -0500

    [FIX]

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const file = require('fs');
 
-file.writeFile(process.argv[2], process.argv[3],(err) => {
+file.writeFile(process.argv[2], process.argv[3], (err) => {
   if (err) {
     throw err;
   }
--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -2,7 +2,7 @@
 const request = require('request');
 request.get(`https://swapi-api.hbtn.io/api/films/${process.argv[2]}`, function (error, response, body) {
   if (error === null) {
-    const { title } = JSON.parse(body)
+    const { title } = JSON.parse(body);
     console.log(title);
   } else {
     console.log(error);
